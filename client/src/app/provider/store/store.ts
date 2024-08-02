ac8c2c51b5332e53672ca8dc7c6d594c9e43b33c
AndreyZagrebelnyy <zaganddmi@gmail.com>
2024-08-02 19:56:33 +0300

    store

--- client/src/app/provider/store/store.ts
+++ client/src/app/provider/store/store.ts
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import { useDispatch, useSelector } from 'react-redux';
 import { authSlice } from '../../../entities/user/authSlice';
 import { eventSlice } from '../../../entities/event/eventSlice';
+import { arenaSlice } from '../../../entities/arena/ArenaSlice';
 
 const store = configureStore({
   reducer: {
     auth: authSlice.reducer,
     events: eventSlice.reducer,
+    arenas: arenaSlice.reducer,
   },
 });
 export type StoreType = typeof store;
