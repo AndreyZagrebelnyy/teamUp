35ffa1f77e0fb89331e23736958ce70f23e4fcb1
AndreyZagrebelnyy <zaganddmi@gmail.com>
2024-08-02 08:31:43 +0300

    navbar created

---
+++ client/src/page/arenas/ArenasPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './ArenasPage.css';
+
+type ArenasPageProps = {
+}
+
+const ArenasPage = ({}: ArenasPageProps): JSX.Element => {
+  return (
+	 <div className='ArenasPage'>
+		Площадки
+	 </div>
+  );
+};
+
+export default ArenasPage;
---
+++ client/src/page/main/MainPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './MainPage.css';
+
+type MainPageProps = {
+}
+
+const MainPage = ({}: MainPageProps): JSX.Element => {
+  return (
+	 <div className='MainPage'>
+		Главная страница
+	 </div>
+  );
+};
+
+export default MainPage;
