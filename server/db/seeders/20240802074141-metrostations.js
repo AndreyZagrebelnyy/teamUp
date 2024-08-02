3265e84ea88d45e558d9ce38d3eb493835ff106a
Hakairyoku <nikischitov@gmail.com>
2024-08-02 14:08:17 +0300

    added user seeds and fixed arenas

--- server/db/seeders/20240802074141-metrostations.js
+++ server/db/seeders/20240802074141-metrostations.js
@@ -4,7 +4,7 @@
 module.exports = {
   async up (queryInterface, Sequelize) {
    await queryInterface.bulkInsert(
-    "Metrostations",
+    "MetroStations",
     [
       {
         title: "Старая Деревня"
@@ -29,6 +29,6 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.bulkDelete("Metrostations", null, {})
+    await queryInterface.bulkDelete("MetroStations", null, {})
   },
 };
