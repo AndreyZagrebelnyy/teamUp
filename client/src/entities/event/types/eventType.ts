bb01a418a29faf214b5aa6aed7969d067776f878
burberryonn <swaronworld2@mail.ru>
2024-08-02 16:45:27 +0300

    feat create Event done

---
+++ client/src/entities/event/types/eventType.ts
@@ -0,0 +1,9 @@
+
+export type Event = {
+    arenaId: number
+    arenaDateId: number
+    sportId: number
+    price: number
+    levelId: number
+    teamSize: number
+}
---
+++ client/src/entities/event/ui/EventItem.tsx
@@ -0,0 +1,8 @@
+import React from'react';
+function EventItem({}):JSX.Element {
+  return (
+      <></>
+  );
+}
+
+export default EventItem;
